4e9917ec0925c15d930bc4cb9bcf9de6a1770e05
osler <extempest1@gmail.com>
2025-11-01 18:27:37 -0600

    inicio

---
+++ componentes.php
@@ -0,0 +1,18 @@
+<?php 
+    function headcontent($title);{
+    echo '<mwt CHARSET="uft-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0>
+        <title>' . $title .' | peliculas</title>
+    
+    
+    
+    <!-- Font Awesome -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+        <!-- MDB -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/9.2.0/mdb.min.css" rel="stylesheet" />
+
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>';
+    }
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+  include 'componentes.php';
+?>
+
+<!DOCTYPE html>
+<html lang="es" data-mdb-theme="light">
+<head>
+    <?php 
+      $title = "Inicio | Proyecto Película";
+      headContent($title);
+    ?>
+</head>
+<body>
+  
+  
+</body>
+</html>
