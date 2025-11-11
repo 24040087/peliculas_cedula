8d7adb20306d2f48451d000c24c2f4236245d3be
24040087 <24040087@alumno.utc.edu.mx>
2025-11-11 13:57:35 -0600

    CAMBIOS: Cambie components, index, main.css, tables.css,. agregue forms, icon, etc

--- php/components.php
+++ php/components.php
@@ -1,17 +1,18 @@
-<?php 
-    function headcontent($title) {
-    echo '<mwt CHARSET="uft-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0>
-        <title>' . $title .' | peliculas</title>
-    
-    
-    
-    <!-- Font Awesome -->
+<?php
+    function headContent($title) {
+        echo '<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="static/img/utc_blue_circle.ico" type="image/x-icon">
+        <title>'.$title.' | Peliculas | Code Crafters</title>
+        
+        <!-- Font Awesome -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
         <!-- MDB -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/9.2.0/mdb.min.css" rel="stylesheet" />
+
+        <link rel="stylesheet" href="static/css/styles.min.css">
 
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>';
     }
@@ -22,6 +23,6 @@
         <!-- SweetAlert2 -->
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-        <script src="static/js/app.min.js"></script>';}
-
+        <script src="static/js/app.min.js"></script>';
+    }
 ?>
